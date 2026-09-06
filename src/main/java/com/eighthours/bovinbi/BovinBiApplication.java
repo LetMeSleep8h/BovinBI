91b7fb8ab48cfafec47e419e6834e6ee91e07cf8
pengzeyu <dhsm20230725@163.com>
2026-09-06 16:42:50 +0800

    项目初始化

---
+++ src/main/java/com/eighthours/bovinbi/BovinBiApplication.java
@@ -0,0 +1,13 @@
+package com.eighthours.bovinbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BovinBiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BovinBiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eighthours/bovinbi/BovinBiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eighthours.bovinbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BovinBiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
